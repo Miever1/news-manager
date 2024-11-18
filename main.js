465e359f78de46fd785d63b6b77273385c291b67
miever <miever1@163.com>
2024-11-18 17:37:08 +0100

    feat: add Electron support
    - Integrated Electron for desktop functionality.
    - Configured dynamic loading for dev and production environments.

---
+++ main.js
@@ -0,0 +1,33 @@
+const { app, BrowserWindow } = require('electron');
+const path = require('path');
+
+let mainWindow;
+
+app.on('ready', () => {
+    mainWindow = new BrowserWindow({
+        width: 800,
+        height: 600,
+        webPreferences: {
+            contextIsolation: true,
+            nodeIntegration: false,
+        },
+    });
+
+    const isDev = process.env.NODE_ENV === 'development';
+
+    if (isDev) {
+        mainWindow.loadURL('http://localhost:4200');
+    } else {
+        mainWindow.loadFile(path.join(__dirname, 'dist/news-manager-project/browser/index.html'));
+    }
+
+    mainWindow.on('closed', () => {
+        mainWindow = null;
+    });
+});
+
+app.on('window-all-closed', () => {
+    if (process.platform !== 'darwin') {
+        app.quit();
+    }
+});
